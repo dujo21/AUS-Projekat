a51630ab1ae6c3b73902e1133f1ac428f68c668d
Dusan <Dusan@DESKTOP-568CDLN>
2024-04-14 23:19:35 +0200

    Add project files.

---
+++ Modbus/ModbusFunctions/WriteSingleRegisterFunction.cs
@@ -0,0 +1,59 @@
+﻿using Common;
+using Modbus.FunctionParameters;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection;
+
+namespace Modbus.ModbusFunctions
+{
+    /// <summary>
+    /// Class containing logic for parsing and packing modbus write single register functions/requests.
+    /// </summary>
+    public class WriteSingleRegisterFunction : ModbusFunction
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WriteSingleRegisterFunction"/> class.
+        /// </summary>
+        /// <param name="commandParameters">The modbus command parameters.</param>
+        public WriteSingleRegisterFunction(ModbusCommandParameters commandParameters) : base(commandParameters)
+        {
+            CheckArguments(MethodBase.GetCurrentMethod(), typeof(ModbusWriteCommandParameters));
+        }
+
+        /// <inheritdoc />
+        public override byte[] PackRequest()
+        {
+            //TO DO: IMPLEMENT
+            //throw new NotImplementedException();
+
+            byte[] request = new byte[12];
+            ModbusWriteCommandParameters modbusWrite = this.CommandParameters as ModbusWriteCommandParameters;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)modbusWrite.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)modbusWrite.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)modbusWrite.Length)), 0, request, 4, 2);
+            request[6] = modbusWrite.UnitId;
+            request[7] = modbusWrite.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)modbusWrite.OutputAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)modbusWrite.Value)), 0, request, 10, 2);
+            return request;
+        }
+
+        /// <inheritdoc />
+        public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
+        {
+            //TO DO: IMPLEMENT
+            //throw new NotImplementedException();
+
+            ModbusWriteCommandParameters modbusWrite = this.CommandParameters as ModbusWriteCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> ret = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            var adresa = BitConverter.ToUInt16(response, 8);
+            var vrednost = BitConverter.ToUInt16(response, 10);
+
+            ret.Add(new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, adresa), vrednost);
+
+            return ret;
+        }
+    }
+}
